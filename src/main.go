ded276168d76e62b478951c98e24e5dbfcbec549
I Wayan Vendy Wiranatha <vendywira@gmail.com>
2019-09-28 12:18:16 +0700

    learn html template

--- src/main.go
+++ src/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"path"
 )
 
-func main()  {
+func main() {
 	http.HandleFunc("/", handlerIndex)
 	http.HandleFunc("/greeting", handlerGreeting)
 
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
 
 	var address = "localhost:8080"
 	fmt.Printf("server started at %s\n", address)
@@ -22,5 +25,20 @@ func handlerGreeting(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlerIndex(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello world"))
+	var filepath = path.Join("views", "index.html")
+	var tmpl, err = template.ParseFiles(filepath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var data = map[string]interface{}{
+		"title": "Learning Golang Web",
+		"name":  "Vendy Wiranatha",
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
